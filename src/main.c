6bf7969f12fea66f58948a5bc9778c1030aad059
Adrien Burgun <adrien.burgun@orange.fr>
2020-05-11 22:26:27 +0200

    :art: Reformat code (pre-merge)

--- src/main.c
+++ src/main.c
@@ -1,10 +1,11 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include "ai.h"
 #include "display.h"
 #include "rules.h"
-#include "ai.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]) {
-    Teams active_team; //variable that will help each time a function needs to know what team is concerned, e.g.: for the contracts
+  Teams active_team; // variable that will help each time a function needs to
+                     // know what team is concerned, e.g.: for the contracts
   printf("Hello world! ");
 }
--- src/rules.h
+++ src/rules.h
@@ -3,12 +3,7 @@
 
 #include <inttypes.h>
 
-enum CardColor {
-  HEARTS,
-  TILES,
-  CLOVERS,
-  SPIKES
-};
+enum CardColor { HEARTS, TILES, CLOVERS, SPIKES };
 typedef enum CardColor CardColor;
 
 /** Represents a single card
@@ -33,52 +28,55 @@ typedef struct Card Card;
 struct Player {
   Card cards[8];
   uint8_t n_cards; // number of cards currently held
-  int trick_points_total; //variable enabling a count of the points won each trick rather than having to stock all the cards won until the end of the 8 tricks
-  int declaration_points; //variable to count the declaration that the player did. If the team of the player does not have the highest value declaration, then this variable will be equal to 0.
-  int tricks_won; //counts the number of tricks won by each player for the capot and general contracts
+  int trick_points_total; // variable enabling a count of the points won each
+                          // trick rather than having to stock all the cards won
+                          // until the end of the 8 tricks
+  int declaration_points; // variable to count the declaration that the player
+                          // did. If the team of the player does not have the
+                          // highest value declaration, then this variable will
+                          // be equal to 0.
+  int tricks_won; // counts the number of tricks won by each player for the
+                  // capot and general contracts
   int belote; // =0 if no belot, = 20 if belot
 };
 typedef struct Player Player;
 
-enum ContractType {
-CHOSENCOLOUR,
-COINCHE,
-SURCOINCHE,
-CAPOT,
-GENERAL
-};
+enum ContractType { CHOSENCOLOUR, COINCHE, SURCOINCHE, CAPOT, GENERAL };
 typedef enum ContractType ContractType;
 
-enum TrumpColor {
-  HEART,
-  TILE,
-  CLOVER,
-  SPIKE,
-  ALLTRUMP,
-  NOTRUMP
-};
+enum TrumpColor { HEART, TILE, CLOVER, SPIKE, ALLTRUMP, NOTRUMP };
 typedef enum TrumpColor TrumpColor;
 
 enum Teams {
   EW, // team east/west, double AI
-  NS, //team south/north, player's team
+  NS, // team south/north, player's team
 };
 typedef enum Teams Teams;
 
 struct Game {
   Player players[4];
   ContractType active_contract;
-  int contract_points; //only if the active contract is a chosen colour, capot=250 or general=500. Otherwise =0
-  TrumpColor active_trump; //to stock the active trump
+  int contract_points; // only if the active contract is a chosen colour,
+                       // capot=250 or general=500. Otherwise =0
+  TrumpColor active_trump; // to stock the active trump
   Teams winning_team;
   Teams contracted_team;
 };
 typedef struct Game Game;
 
-int turn_points (Game game, Player player1, Player player2,Teams active_team,Player looser1,Player looser2);
-/*The goal of this function is to compute the points of a team at the end of a 8 tricks turn.
-To do so, we give all the informations about the game and the 4 players to the function. The function will then return the points of the wanted team which is an integer.
-This function doesn't check if the contract was realized or not, at this point we already know it. An other function will do it.
+/**
+The goal of this function is to compute the points of a team at the end of a 8
+tricks turn. To do so, we give all the informations about the game and the 4
+players to the function. The function will then return the points of the wanted
+team which is an integer. This function doesn't check if the contract was
+realized or not, at this point we already know it. Another function will do it.
 */
+int turn_points(
+  Game game,
+  Player player1,
+  Player player2,
+  Teams active_team,
+  Player looser1,
+  Player looser2);
 
 #endif // RULES_H
